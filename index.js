2814986f161632918150c27483f9195390b2c0df
MarkNjunge <mark.kamau@outlook.com>
2023-08-16 13:34:31 +0300

    Remove dependency on @marknjunge/eslint-config

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const eslintrc = require("./.eslintrc.js");
-const base = require("@marknjunge/eslint-config")
 
-module.exports = { 
-  rules: { ...base.rules, ...eslintrc.rules } 
+module.exports = {
+  rules: eslintrc.rules,
 };
